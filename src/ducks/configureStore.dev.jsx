508bfb96448534c4bbedeb37b50edce8f0b83300
Dan Lubbers <lubbers.dan@gmail.com>
2020-04-29 00:08:32 -0400

    Finished module Production Builds

---
+++ src/ducks/configureStore.dev.jsx
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers/index.jsx";
+// This warns us if we accidentally mutate state in the redux store
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
+
+export default function configureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // Add support for Redux dev tools
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
+  );
+}
